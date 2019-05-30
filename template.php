8732c0ae9c556218a6d1b48b796ee473bf0f862b
bourn404 <bourn404@gmail.com>
2019-05-30 16:36:59 -0600

    Created a template page

---
+++ template.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/wdd/common/head.php'; ?>
+    <title>Home | WDD</title>
+</head>
+
+<body>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . '/wdd/common/header.php'; ?>
+    <main>
+        <h1>My Content Title</h1>
+        <p>Page content goes here.</p>
+    </main>
+    <footer>
+        <?php include $_SERVER['DOCUMENT_ROOT'] . '/wdd/common/footer.php'; ?>
+    </footer>
+    <script src="/wdd/js/core.js"></script>
+</body>
+
+</html>

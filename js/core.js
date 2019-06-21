e32e90c1bcf56aa7fd19e29ce5e432e4656ffcab
UserStefDLT <UserStefDLT@gmail.com>
2019-06-20 18:32:16 -0600

    Stefano Comminiting Again.

---
+++ js/core.js
@@ -0,0 +1,21 @@
+//-- What is WDD? --//
+$(document).ready(function() {
+    $( "#rest-Web" ).delay( 2000 ).animate({
+        opacity: 0,
+        width: "-=100%",
+    }, 3000,
+    );
+
+    $( "#rest-Design" ).delay( 2000 ).animate({
+        opacity: 0,
+        width: "-=100%",
+    }, 3000, 
+    );
+
+    $( "#rest-Development" ).delay( 2000 ).animate({
+        opacity: 0,
+        width: "-=100%",
+    }, 3000, 
+    );
+});
+//-- (end of) What is WDD? --//

bce14c7b913c373d6a70702703cf0176ee427c77
bourn404 <bourn404@gmail.com>
2019-07-11 18:46:02 -0600

    added a mobile menu

--- js/core.js
+++ js/core.js
@@ -1,21 +1,39 @@
 //-- What is WDD? --//
 $(document).ready(function() {
-    $( "#rest-Web" ).delay( 2000 ).animate({
-        opacity: 0,
-        width: "-=100%",
-    }, 3000,
-    );
+    $("#rest-Web")
+        .delay(2000)
+        .animate(
+            {
+                opacity: 0,
+                width: "-=100%"
+            },
+            3000
+        );
 
-    $( "#rest-Design" ).delay( 2000 ).animate({
-        opacity: 0,
-        width: "-=100%",
-    }, 3000, 
-    );
+    $("#rest-Design")
+        .delay(2000)
+        .animate(
+            {
+                opacity: 0,
+                width: "-=100%"
+            },
+            3000
+        );
 
-    $( "#rest-Development" ).delay( 2000 ).animate({
-        opacity: 0,
-        width: "-=100%",
-    }, 3000, 
-    );
+    $("#rest-Development")
+        .delay(2000)
+        .animate(
+            {
+                opacity: 0,
+                width: "-=100%"
+            },
+            3000
+        );
 });
 //-- (end of) What is WDD? --//
+var clicks = 0;
+document.querySelector("#nav-toggle").addEventListener("click", function() {
+    this.parentNode.classList.toggle("open");
+    clicks++;
+    console.log(clicks);
+});

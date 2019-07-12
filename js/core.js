11ab863d5584ac92a3aac2bdab4b00135f571a6c
UserStefDLT <UserStefDLT@gmail.com>
2019-07-11 23:26:29 -0600

    one hourse added - pops and then leaves.

--- js/core.js
+++ js/core.js
@@ -2,36 +2,22 @@
 $(document).ready(function() {
     $("#wdd-web-rest")
         .delay(3000)
-        .animate(
-            {
-                opacity: 0,
-                width: 0
-            },
-            2000
-        )
+        .animate({opacity: 0, width: 0}, 2000)
         .fadeOut(0);
-
     $("#wdd-des-rest")
         .delay(3000)
-        .animate(
-            {
-                opacity: 0,
-                width: 0
-            },
-            2000
-        )
+        .animate({opacity: 0, width: 0}, 2000)
         .fadeOut(0);
-
     $("#wdd-dev-rest")
         .delay(3000)
-        .animate(
-            {
-                opacity: 0,
-                width: 0
-            },
-            2000
-        )
+        .animate({opacity: 0, width: 0}, 2000)
         .fadeOut(0);
+
+    $("#hoursesImg")
+        .delay(1000)
+        .animate({right: "-40px"}, 2000)
+        .delay(5000)
+        .animate({right: "-310px"}, 2000);
 });
 //-- (end of) What is WDD? --//
 var clicks = 0;
